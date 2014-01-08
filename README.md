9fc2f667cfbc5047416361fc2334f9510f16575e
Jon Schlinkert <dev@sellside.com>
2014-01-07 19:46:37 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# cwd [![NPM version](https://badge.fury.io/js/cwd.png)](http://badge.fury.io/js/cwd)
+
+> Get the current working directory relative to the calling module.
+
+## Quickstart
+
+```js
+var cwd = require('cwd');
+```
+
+## Usage
+
+```js
+var pkg = require(path.join(cwd, 'package.json'));
+```
+
+## Author
+
+**Jon Schlinkert**
+
++ [github/jonschlinkert](https://github.com/jonschlinkert)
++ [twitter/jonschlinkert](http://twitter.com/jonschlinkert)
+
+## License
+Copyright (c) 2014 Jon Schlinkert, contributors.
+Released under the MIT license
---
+++ index.js
@@ -0,0 +1,19 @@
+/**
+ * cwd
+ * https://github.com/jonschlinkert/cwd
+ * Copyright (c) 2013 Jon Schlinkert, contributors.
+ * Licensed under the MIT license.
+ */
+
+var path = require('path');
+var stacktrace = require('stack-trace');
+var findup = require('findup-sync');
+
+// Inspired by the loadConfig function in matchdep:
+// https://github.com/tkellen/node-matchdep
+//
+// "The calling module's path. Unfortunately, because modules are cached,
+// module.parent is the FIRST calling module parent, not necessarily the
+// current one". - @cowboy
+var callerPath = path.dirname(stacktrace.get(callerPath)[1].getFileName());
+module.exports = path.dirname(findup('package.json', {cwd: callerPath}));
