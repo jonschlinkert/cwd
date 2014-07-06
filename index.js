ba0daa9ce94131a2f31d7653172bd56d2b1bae20
jonschlinkert <dev@sellside.com>
2014-07-06 11:55:09 -0400

    remove relative

--- index.js
+++ index.js
@@ -7,12 +7,11 @@
 var path = require('path');
 var normalize = require('normalize-path');
 var findup = require('findup-sync');
-var relative = require('relative');
 
 module.exports = function cwd() {
   var filepath = path.join.apply(path, [].slice.call(arguments));
   var base = path.dirname(findup('package.json', {
     cwd: process.cwd()
   }));
-  return relative(path.resolve(base, filepath || ''));
+  return normalize(path.resolve(base, filepath || ''));
 };
