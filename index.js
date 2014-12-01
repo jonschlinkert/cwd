c2c44892b8ce12f6d9b4c28e669b3a550bb853d8
jonschlinkert <dev@sellside.com>
2014-12-01 05:10:29 -0500

    minor refactor, uses look-up

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 /**
  * cwd <https://github.com/jonschlinkert/cwd>
- * Copyright (c) 2014 Jon Schlinkert, contributors.
+ *
+ * Copyright (c) 2014-2015 Jon Schlinkert, contributors.
  * Licensed under the MIT license.
  */
 
 var path = require('path');
-var normalize = require('normalize-path');
-var findup = require('findup-sync');
+var lookup = require('look-up');
 
-module.exports = function cwd() {
-  var filepath = path.join.apply(path, [].slice.call(arguments));
-  var base = path.dirname(findup('package.json', {
-    cwd: process.cwd()
-  }));
-  return normalize(path.resolve(base, filepath || ''));
-};
+/**
+ * Expose `cwd`
+ */
+
+module.exports = cwd;
+
+/**
+ * Uses [look-up] to resolve the absolute path to the root of a project.
+ *
+ * @param {String|Array} `filepath` The starting filepath. Can be a string, or path parts as a list of arguments or array.
+ * @return {String} Resolve filepath.
+ * @api public
+ */
+
+function cwd(filepath) {
+  var fp = path.resolve.apply(path, [].concat.apply([], arguments));
+  try {
+    var res = lookup('package.json', {cwd: fp});
+    var base = res ? path.dirname(res) : '';
+    return path.resolve(base, fp);
+  } catch (err) {
+    return fp;
+  }
+}
