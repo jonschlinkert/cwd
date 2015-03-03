cc01d92f49797df0741d1f9853aa55276d1b164f
jonschlinkert <jon.schlinkert@sellside.com>
2015-03-02 23:02:32 -0500

    implement caching

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-/**
+/*!
  * cwd <https://github.com/jonschlinkert/cwd>
  *
- * Copyright (c) 2014-2015 Jon Schlinkert, contributors.
- * Licensed under the MIT license.
+ * Copyright (c) 2014-2015, Jon Schlinkert.
+ * Licensed under the MIT License.
  */
 
 var path = require('path');
@@ -15,6 +15,13 @@ var lookup = require('look-up');
 module.exports = cwd;
 
 /**
+ * Cache filepaths to prevent hitting the file system
+ * for multiple lookups on the exact same path.
+ */
+
+var cache = {};
+
+/**
  * Uses [look-up] to resolve the absolute path to the root of a project.
  *
  * @param {String|Array} `filepath` The starting filepath. Can be a string, or path parts as a list of arguments or array.
@@ -24,11 +31,15 @@ module.exports = cwd;
 
 function cwd(filepath) {
   var fp = path.resolve.apply(path, [].concat.apply([], arguments));
+  if (cache.hasOwnProperty(fp)) {
+    return cache[fp];
+  }
+
   try {
     var res = lookup('package.json', {cwd: fp});
     var base = res ? path.dirname(res) : '';
-    return path.resolve(base, fp);
+    return (cache[fp] = path.resolve(base, fp));
   } catch (err) {
-    return fp;
+    return (cache[fp] = fp);
   }
 }
