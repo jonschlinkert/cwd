88f3026619144356c6fc5876ca57c6e47d71f43a
jonschlinkert <jon.schlinkert@sellside.com>
2015-10-20 01:34:51 -0400

    adds tests

--- test.js
+++ test.js
@@ -13,10 +13,14 @@ function absolute(fp) {
 describe('cwd:', function() {
   it('should return the absolute filepath to the cwd', function() {
     assert.equal(normalize(cwd()), absolute());
+    assert.equal(normalize(cwd('')), absolute());
+    assert.equal(normalize(cwd(process.cwd())), absolute());
+    assert.equal(normalize(cwd(__dirname)), absolute());
   });
 
   it('should return the absolute filepath to the given file', function() {
     assert.equal(normalize(cwd('package.json')), absolute('package.json'));
+    assert.equal(normalize(cwd(__filename)), absolute('test.js'));
   });
 
   it('should return the absolute path relative to the cwd', function() {
